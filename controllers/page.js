e1c89de56895fe21cfefc868da67cefb9c9a3fbd
Kautsky Lozano <kautskylozanov@outlook.com>
2020-05-21 19:51:17 +0000

    adde demo data

--- controllers/page.js
+++ controllers/page.js
@@ -3,8 +3,7 @@ var sqlite3 = require('sqlite3').verbose();
 exports.getPage = (req, res) => {
   let pageId = req.params.id;
   let db = new sqlite3.Database('./temp/db.db');
-  db.on("trace", s => console.log(s))
-  db.get("SELECT * FROM Pages WHERE id = ? ORDER BY timestamp DESC", [pageId], (e, row) => {
+  db.get("SELECT * FROM Pages WHERE id = ? AND modifiedReason IS NULL ORDER BY timestamp DESC", [pageId], (e, row) => {
     if (row) {
       res.status(200).json(row)
     }
@@ -34,21 +33,40 @@ exports.searchPages = (req, res, next) => {
 
 exports.addPage = (req, res) => {
   let db = new sqlite3.Database('./temp/db.db');
-  let page = req.body;
-  if (page.id) {
-    res.status(200).json(page);
-  } else {
-    const id = page.title.replace(" ", "_");
-    console.log(id);
+  let newPage = req.body;
 
-    db.run("INSERT INTO Pages (id, title, content, imagePath, modifiedReason, timestamp) VALUES (?,?,?,?,?,?)",
-      [id, page.title, page.content, page.imagePath, null, new Date().toISOString()], (err) => {
-        if (err) {
-          res.status(500).json(err);
-        }
-      });
-    res.status(200).json(id)
-  }
+  const id = newPage.title.replace(" ", "_");
 
-  db.close();
+  getPageByTitle(newPage.title).then(foundPage => {
+    if (foundPage) {
+      res.status(409).json();
+    } else {
+      const now = new Date();
+      db.run("INSERT INTO Pages (id, title, content, imagePath, modifiedReason, timestamp) VALUES (?,?,?,?,?,?)",
+        [id, newPage.title, newPage.content, newPage.imagePath, null, now.toISOString()], (err) => {
+          if (err) {
+            res.status(500).json(err);
+          } else {
+            newPage.id = id;
+            newPage.timestamp = now;
+            res.status(200).json(newPage)
+          }
+          db.close();
+        });
+    }
+  }, r => { res.status(500).json(r) });
+}
+
+function getPageByTitle(title) {
+  return new Promise((resolve, reject) => {
+    let db = new sqlite3.Database('./temp/db.db');
+    db.get("SELECT * FROM Pages WHERE title = ? AND modifiedReason IS NULL ORDER BY timestamp DESC", [title], (e, row) => {
+      if (e) {
+        reject(e);
+      } else {
+        resolve(row);
+      }
+      db.close();
+    });
+  });
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require("path")
 const routes = require('./routes/page')
 const app = express();
+const demoData = require("./demo");
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./temp/db.db');
@@ -28,17 +29,8 @@ db.serialize(function () {
             for (let i = total; i < 10; i++) {
                 let id = i + 1;
                 db.run("INSERT OR REPLACE INTO Pages (id, title, content, imagePath, modifiedReason, timestamp) VALUES (?,?,?,?,?,?)", `Title_${id}`,
-                    `Title ${id}`,
-                    `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
-              Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.
-              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
-              Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
-              Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.
-              Fusce aliquet pede non pede. Suspendisse dapibus lorem pellentesque magna. Integer nulla.
-              Donec blandit feugiat ligula. Donec hendrerit, felis et imperdiet euismod, purus ipsum pretium metus, in lacinia nulla nisl eget sapien. Donec ut est in lectus consequat consequat.
-              Etiam eget dui. Aliquam erat volutpat. Sed at lorem in nunc porta tristique.
-              Proin nec augue. Quisque aliquam tempor magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-              Nunc ac magna. Maecenas odio dolor, vulputate vel, auctor ac, accumsan id, felis. Pellentesque cursus sagittis felis.`,
+                    `Title ${id}`, 
+                    demoData.demoContent,
                     'defaultImage',
                     'wrong title',
                     date.toISOString()                    
@@ -46,16 +38,7 @@ db.serialize(function () {
 
                     db.run("INSERT OR REPLACE INTO Pages (id, title, content, imagePath, modifiedReason, timestamp) VALUES (?,?,?,?,?,?)", `Title_${id}`,
                     `Title ${id} modified`,
-                    `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
-              Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.
-              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
-              Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
-              Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.
-              Fusce aliquet pede non pede. Suspendisse dapibus lorem pellentesque magna. Integer nulla.
-              Donec blandit feugiat ligula. Donec hendrerit, felis et imperdiet euismod, purus ipsum pretium metus, in lacinia nulla nisl eget sapien. Donec ut est in lectus consequat consequat.
-              Etiam eget dui. Aliquam erat volutpat. Sed at lorem in nunc porta tristique.
-              Proin nec augue. Quisque aliquam tempor magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-              Nunc ac magna. Maecenas odio dolor, vulputate vel, auctor ac, accumsan id, felis. Pellentesque cursus sagittis felis.`,
+                    demoData.demoContent,
                     'defaultImage',
                     null,
                     endDate.toISOString(),
